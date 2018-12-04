8c244a01265851b87a5f0cc7b8f7b066d9bf491d
Abhay Donthi <adonthi@gmail.com>
2018-12-04 13:28:48 -0500

    added some possible dropdown box from w3schools, needs to be edited. Also added title in top left

--- notkayak.js
+++ notkayak.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
 });
 
 var build_home = function() {
-    $('#title_text').text('Home');
+    $('#title_text').text('Find your flight here!');
     $('a[isActive=true]').attr('isActive', false);
     $('.home_nav').attr('isActive', true);
     $('#content').empty();
@@ -14,13 +14,24 @@ var build_home = function() {
 
 var build_navbar = function() {
     $('#navbar').append('<ul id="navbar_list"></ul>')
-    $('#navbar_list').append('<li><a class="home_nav" isActive=false onclick="build_home()">Home</a></li>')
+    $('#navbar_list').append('<li><p id="title">NOT KAYAK</p></li>')
+    .append('<li><a class="home_nav" isActive=false onclick="build_home()">Home</a></li>')
     .append('<li><a class="flights_nav" isActive=false onclick="build_my_flights()">My Flights</a></li>')
     .append('<li><a class="flight_view_nav" isActive=false onclick="build_flight_view()">Flight View</a></li>');
 }
 
 var build_search = function() {
-
+    $('#search').append('<div id="dropdowns"></div>');
+    $('#dropdowns').append(
+    '<div class="dropdown"> \
+        <button onclick="myFunction()" class="dropbtn">Dropdown</button> \
+        <div id="myDropdown" class="dropdown-content"> \
+            <a href="#">Link 1</a> \
+            <a href="#">Link 2</a> \
+            <a href="#">Link 3</a> \
+        </div> \
+    </div>');
+    
 }
 
 var build_my_flights = function() {
@@ -33,4 +44,25 @@ var build_flight_view = function() {
     console.log("calling build_flight_view()");
     $('a[isActive=true]').attr('isActive', false);
     $('.flight_view_nav').attr('isActive', true);
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
